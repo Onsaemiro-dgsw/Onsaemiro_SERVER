32d254957eb80bbac8cd9b22801aec223d6a0ec4
tlsalsco <tlsalsco@dgsw.hs.kr>
2024-07-17 09:42:35 +0900

    first commit

---
+++ src/main/java/com/dgsw/onsaemiro/OnsaemiroApplication.java
@@ -0,0 +1,13 @@
+package com.dgsw.onsaemiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnsaemiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnsaemiroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dgsw/onsaemiro/OnsaemiroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgsw.onsaemiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnsaemiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
